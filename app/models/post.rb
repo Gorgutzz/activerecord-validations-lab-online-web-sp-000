6b519c1d3aa20d2d6f6deae935c7b962cca15ea6
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-07-28 02:55:15 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,10 +5,10 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: {in: %w(Fiction Non-fiction)}
   validate :click_baity
 
-Click_bait = [/Won't Believe/i,
-/Secret/i,
-/Top [0-9]*/i,
-/Guess/i]
+  Click_bait = [/Won't Believe/i,
+  /Secret/i,
+  /Top [0-9]*/i,
+  /Guess/i]
 
   def click_baity
       if Click_bait.none? {|p| p.match title}
